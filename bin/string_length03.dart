9e8898cdadfe0991f699f047d472bc390917eeb0
Begzod <ebegzod477@gmail.com>
2022-07-02 22:03:28 +0500

    code03

---
+++ bin/string_length03.dart
@@ -0,0 +1,13 @@
+bool func(String a,b){
+  int x,y;
+  x=a.length;
+  y=b.length;
+  if(x==y){
+     return true;
+  }else
+  return false;
+ 
+}
+void main(){
+  print(func('salom','salomlar'));
+}
